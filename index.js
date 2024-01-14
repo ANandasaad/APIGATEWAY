cd0887f6ec4be2632371e9290621851d8189f263
Anand kushwaha <anandkushwaha@Anands-Mac-mini.local>
2024-01-15 01:20:39 +0530

    api gateway updated

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 const bodyParser = require("body-parser");
+const rateLimit = require("express-rate-limit");
 const express = require("express");
 const morgan = require("morgan");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const axios = require("axios");
+
 const app = express();
 const PORT = 3005;
+
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000,
+  limit: 5,
+});
 app.use(morgan("combined"));
+app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/authservice", async (req, res, next) => {
+  try {
+    // console.log(req.headers["x-access-token"]);
+    const response = await axios.get(
+      "http://localhost:3001/authservice/api/v1/isAuthenticated",
+      {
+        headers: {
+          "x-access-token": req.headers["x-access-token"],
+        },
+      }
+    );
+    console.log("Response:", response.data);
+    if (response.data.success) {
+      next();
+    } else {
+      return res.status(401).json({
+        message: "Authentication failed",
+      });
+    }
+  } catch (error) {
+    res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+});
+
 app.use(
   "/bookingservice",
   createProxyMiddleware({
@@ -14,12 +50,13 @@ app.use(
     changeOrigin: true,
   })
 );
-app.get("/home", function (req, res) {
-  res.json({
-    success: true,
-    message: "Welcome",
-  });
-});
+app.use(
+  "/authservice",
+  createProxyMiddleware({
+    target: "http://localhost:3001/",
+    changeOrigin: true,
+  })
+);
 app.listen(PORT, () => {
   console.log("listening on port " + PORT);
 });
